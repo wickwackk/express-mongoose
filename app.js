d354fb48a5b8b885773f102dd1f49a6b66cc8876
Morning <>
2023-03-20 12:10:15 +0800

    mar20

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
 
+const cloud = require("./config/cloudinary-config.js");
 const db = require("./config/mongoose-config.js");
 const Movie = require("./model/Movie");
 const movie_router = require("./routes/movies-api.js");
@@ -32,6 +34,21 @@ app.use(movie_router);
 // db.on("error", (error) => {
 //   console.log("error connecting", error);
 // });
+
+const res = cloudinary.uploader.upload("/Users/morning/Downloads/andy.jpeg", {
+  public_id: "hey2",
+  folder: "Andy bagsh",
+  use_filename: true,
+});
+
+res
+  .then((data) => {
+    console.log(data);
+    console.log(data.secure_url);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(2030, () => {
   console.log("listening at 2030");
---
+++ config/cloudinary-config.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2;
+
+// Configuration
+const cloud = cloudinary.config({
+  cloud_name: "dcyy3kzv4",
+  api_key: "399849566432533",
+  api_secret: "cJQixFmve4BbDI79WjAja32-AAs",
+});
+
+module.exports = cloud;
